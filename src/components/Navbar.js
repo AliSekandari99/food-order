fe36e77cc6f5855a534d6be1a0c40625df0f97db
ali janzz <mr.a.sekandari@gmail.com>
2022-04-16 16:39:55 +0430

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import ReorderIcon from "@material-ui/icons/Reorder";
+import "../styles/Navbar.css";
+const Navbar = () => {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+  return (
+    <div className="navbar">
+      <div className="leftSide" id={openLinks ? "open" : "close"}>
+        <img src={Logo} alt="" />
+        <div className="hiddenLinks">
+          <Link to="/"> Home </Link>
+          <Link to="/menu"> Menu </Link>
+          <Link to="/about"> About </Link>
+          <Link to="/contact"> Contact </Link>
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      </div>
+      <div className="rightside">
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/about"> About </Link>
+        <Link to="/contact"> Contact </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
